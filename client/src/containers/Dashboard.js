9df4240107ce2a89fb2610309ed75e432a8556f3
Pat Rivet <patrickmrivet@gmail.com>
2020-08-24 18:11:27 +0100

    Client: Add 30 mins timeout for JWT and add authenticated home routing

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -7,7 +7,22 @@ import Header from './../components/Header';
 import Register from './../components/Register';
 
 export default function Dashboard (props) {
-  const [isAuth, setIsAuth] = useState(() => localStorage.getItem("auth"));
+
+  const authCheck = () => {
+    // Get accessToken from localStorage and check if expired.
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) return false;
+    // Extract the 2nd part of token - payload (json)
+    const accessTokenPayload = atob(accessToken.split('.')[1]);
+    const iat = JSON.parse(accessTokenPayload).iat;
+
+    // check curent time is <= the issued at time (iat) + 30 mins
+    // 30 mins as millis // ((1000 * 60)*30);
+    const expireTime = (iat * 1000) + ((1000 * 60)*30);
+    return (Date.now() <= expireTime);
+  };
+
+  const [isAuth, setIsAuth] = useState(() => authCheck());
 
   return (
       <Switch>
@@ -23,9 +38,20 @@ export default function Dashboard (props) {
 
         <Route path='/register' component={Register}/>
 
+        {/* Login route */}
         {!isAuth &&
           <Route path='/' render={ (props) => (
             <Login {...props} setIsAuth={setIsAuth}/>
+          )}/>
+        }
+
+        {/* Authenticated base route */}
+        {isAuth &&
+          <Route path='/' render={ (props) => (
+            <>
+              <Header {...props} setIsAuth={setIsAuth} />
+              <App {...props} />
+            </>
           )}/>
         }
       </Switch>
